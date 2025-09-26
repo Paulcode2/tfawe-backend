9df4b327d922b14b337d7edd6eb7bb5428b6183b
Paul <paullevites84@gmail.com>
2025-09-26 14:18:31 +0100

    fixes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      // "https://tfawe-frontend.onrender.com",
+      "https://tfawe-frontend.onrender.com",
       "https://tfawe-revamp.vercel.app",
     ],
     credentials: true,
--- server.js
+++ server.js
@@ -7,13 +7,15 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tfawe";
 
+console.log("Starting server...");
+console.log("Using Mongo URI:", MONGO_URI);
+console.log("Port:", PORT);
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
